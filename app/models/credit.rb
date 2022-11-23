444018c38eca43cd841079f8c145767ac1a41c8a
Thibault Chassine <thibault.chassine@gmail.com>
2022-11-23 16:20:32 +0100

    remove before save and after initialize as it was not working for calcul

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -1,16 +1,12 @@
 class Credit < ApplicationRecord
   validates :borrowed_amount, :duration, :nominal_rate, :guarante_fee,
             :application_fee, :insurance_rate, presence: true
-  before_save :convert_rate_percentage
-  after_initialize do
-    self.nominal_rate = (nominal_rate * 100).round(2)
-    self.insurance_rate = (insurance_rate * 100).round(2)
+
+  def insurance_percentage
+    insurance_rate / 100
   end
 
-  private
-
-  def convert_rate_percentage
-    self.nominal_rate = nominal_rate / 100
-    self.insurance_rate = insurance_rate / 100
+  def nominal_percentage
+    nominal_rate / 100
   end
 end
