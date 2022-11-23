b541c3df24260ac6b4cf864266b86d6bc9209a94
Thibault Chassine <thibault.chassine@gmail.com>
2022-11-23 15:55:36 +0100

    refacto method

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -1,16 +1,12 @@
 class Credit < ApplicationRecord
   validates :borrowed_amount, :duration, :nominal_rate, :guarante_fee,
             :application_fee, :insurance_rate, presence: true
-  before_save :convert_nominal_rate_percentage
-  before_save :convert_insurance_rate_percentage
+  before_save :convert_rate_percentage
 
   private
 
-  def convert_nominal_rate_percentage
+  def convert_rate_percentage
     self.nominal_rate = nominal_rate / 100
-  end
-
-  def convert_insurance_rate_percentage
     self.insurance_rate = insurance_rate / 100
   end
 end
