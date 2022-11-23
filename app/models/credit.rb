4f7d4b893b5a71b2bd26230b6b8735ab71462627
Thibault Chassine <thibault.chassine@gmail.com>
2022-11-23 15:53:47 +0100

    add convert rates in percentage

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -1,4 +1,16 @@
 class Credit < ApplicationRecord
   validates :borrowed_amount, :duration, :nominal_rate, :guarante_fee,
             :application_fee, :insurance_rate, presence: true
+  before_save :convert_nominal_rate_percentage
+  before_save :convert_insurance_rate_percentage
+
+  private
+
+  def convert_nominal_rate_percentage
+    self.nominal_rate = nominal_rate / 100
+  end
+
+  def convert_insurance_rate_percentage
+    self.insurance_rate = insurance_rate / 100
+  end
 end
