18f1b1834934bb0b587d9aa96c86d42a3318dc47
Thibault Chassine <thibault.chassine@gmail.com>
2022-11-23 16:24:26 +0100

    added method to calculate insurance cost

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -9,4 +9,8 @@ class Credit < ApplicationRecord
   def nominal_percentage
     nominal_rate / 100
   end
+
+  def insurance_cost
+    borrowed_amount * insurance_percentage * duration / 12
+  end
 end
