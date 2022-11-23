16ba5e36eac0133e965de758630a335514d263bb
Thibault Chassine <thibault.chassine@gmail.com>
2022-11-23 15:59:06 +0100

    added initialize to read rate as percentage

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -2,6 +2,10 @@ class Credit < ApplicationRecord
   validates :borrowed_amount, :duration, :nominal_rate, :guarante_fee,
             :application_fee, :insurance_rate, presence: true
   before_save :convert_rate_percentage
+  after_initialize do
+    self.nominal_rate = (nominal_rate * 100).round(2)
+    self.insurance_rate = (insurance_rate * 100).round(2)
+  end
 
   private
 
