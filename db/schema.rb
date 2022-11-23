eec2a754ee273dd61d26e34d342408667edacd85
Thibault Chassine <thibault.chassine@gmail.com>
2022-11-23 15:01:59 +0100

    created db and add migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_23_140008) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "credits", force: :cascade do |t|
+    t.integer "borrowed_amount"
+    t.integer "duration"
+    t.float "nominal_rate"
+    t.integer "guarante_fee"
+    t.integer "application_fee"
+    t.float "insurance_rate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
