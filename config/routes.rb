b7d0eac17e7cfa5a03a21731d6426dd57366e70b
Thibault Chassine <thibault.chassine@gmail.com>
2022-11-24 04:27:24 +0100

    added route for show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  resources :credits, only: :create
+  resources :credits, only: %i[create show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
