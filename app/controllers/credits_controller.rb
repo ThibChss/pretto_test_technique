b2fe29ff41cceb8cacd3f46afface904197358c4
Thibault Chassine <thibault.chassine@gmail.com>
2022-11-24 04:08:24 +0100

    added form on home to test if create work

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -6,7 +6,7 @@ class CreditsController < ApplicationController
   private
 
   def credit_params
-    params.require(:credit).authorize(:borrowed_amount, :duration, :nominal_rate, :guarantee_fee,
+    params.require(:credit).permit(:borrowed_amount, :duration, :nominal_rate, :guarantee_fee,
                                       :application_fee, :brokerage_fee, :insurance_rate)
   end
 end
