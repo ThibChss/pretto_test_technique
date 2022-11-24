8c770deaf38aa43ead25366d031b9686a41b1707
Thibault Chassine <thibault.chassine@gmail.com>
2022-11-24 03:58:33 +0100

    added params authorization

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -1,2 +1,8 @@
 class CreditsController < ApplicationController
+  private
+
+  def credit_params
+    params.require(:credit).authorize(:borrowed_amount, :duration, :nominal_rate, :guarantee_fee,
+                                      :application_fee, :brokerage_fee, :insurance_rate)
+  end
 end
