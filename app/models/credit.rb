1dbf9aad9f83415731ecf95d1fb9e7fc22d70227
Thibault Chassine <thibault.chassine@gmail.com>
2022-11-24 03:51:44 +0100

    first try of calculate taeg not working

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -2,6 +2,11 @@ class Credit < ApplicationRecord
   validates :borrowed_amount, :duration, :nominal_rate, :guarantee_fee,
             :application_fee, :brokerage_fee, :insurance_rate, presence: true
 
+  # Convert months in years
+  def duration_in_years
+    duration / 12
+  end
+
   # Convert insurance rate in percentage for calcul
   def insurance_percentage
     insurance_rate / 100
@@ -14,12 +19,17 @@ class Credit < ApplicationRecord
 
   # Calculate total cost of insurance
   def insurance_cost
-    borrowed_amount * insurance_percentage * duration / 12
+    borrowed_amount * insurance_percentage * duration_in_years
   end
 
-  # Calculate amount to be refund monthly not including insurance and interest
+  # Calculate amount to be refund monthly not including insurance
   def monthly_instalment
-    borrowed_amount * ((nominal_percentage / 12) / (1 - ((1 + (nominal_percentage / 12))**-duration)))
+    (borrowed_amount * ((nominal_percentage / 12) / (1 - ((1 + (nominal_percentage / 12))**-duration)))).round(2)
+  end
+
+  # Calculate amount to be refund monthly including insurance
+  def monthly_instalment_w_insurance
+    monthly_instalment + (insurance_cost / duration)
   end
 
   # Calculate total amount of interest
@@ -27,13 +37,23 @@ class Credit < ApplicationRecord
     (duration * monthly_instalment) - borrowed_amount
   end
 
-  # Calculate the total cost of the credit
+  # Calculate the total cost of the credit without insurance
   def credit_cost
     interests_amount + guarantee_fee + application_fee + brokerage_fee
   end
 
+  # Calculate the total cost of the credit including insurance
+  def credit_cost_w_insurance
+    credit_cost + insurance_cost
+  end
+
   # Calculate the total amount to refund
   def include_all_fee
-    borrowed_amount + credit_cost + insurance_cost
+    borrowed_amount + credit_cost_w_insurance
+  end
+
+  # Calculate the annual percentage rate of charge
+  def calculate_aprc
+    (include_all_fee - borrowed_amount) / borrowed_amount * duration / duration_in_years
   end
 end
