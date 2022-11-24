e68787276c7af94dc7ed1c26f8c1a1c19b3b5991
Thibault Chassine <thibault.chassine@gmail.com>
2022-11-24 11:09:06 +0100

    added method js controller to display form

--- app/javascript/controllers/display_form_controller.js
+++ app/javascript/controllers/display_form_controller.js
@@ -2,6 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="display-form"
 export default class extends Controller {
+  static targets = ['buttonHome', 'formDiv']
+
   connect() {
+    console.log('Connected');
+  }
+
+  display_form(event) {
+    event.preventDefault()
+    this.buttonHomeTarget.classList.add('d-none')
+    this.formDivTarget.classList.remove('d-none')
   }
 }
